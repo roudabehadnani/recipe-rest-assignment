708a9eea8f2b80361d6cf292a64bea82bde2c952
roudabehadnani <rodabeh.adnani@gmail.com>
2022-10-25 23:13:18 +0200

    exception

---
+++ src/main/java/com/example/reciperestassignment/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.reciperestassignment.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+    }
+}
