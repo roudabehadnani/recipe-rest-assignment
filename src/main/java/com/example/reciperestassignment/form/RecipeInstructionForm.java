7667fcdbfdae683b9d14243d995085c30bab0865
roudabehadnani <rodabeh.adnani@gmail.com>
2022-10-30 23:03:02 +0100

    Form Package added

---
+++ src/main/java/com/example/reciperestassignment/form/RecipeInstructionForm.java
@@ -0,0 +1,14 @@
+package com.example.reciperestassignment.form;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RecipeInstructionForm {
+
+    private String instruction;
+
+}
