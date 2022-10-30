d999726180079068cd4ab6960781c79c2968613b
roudabehadnani <rodabeh.adnani@gmail.com>
2022-10-30 23:04:21 +0100

    Recipe Controller

--- src/main/java/com/example/reciperestassignment/controllers/RecipeController.java
+++ src/main/java/com/example/reciperestassignment/controllers/RecipeController.java
@@ -1,7 +1,9 @@
 package com.example.reciperestassignment.controllers;
 
+import com.example.reciperestassignment.dto.RecipeCategoryDto;
 import com.example.reciperestassignment.dto.RecipeDto;
-import com.example.reciperestassignment.services.RecipeService;
+import com.example.reciperestassignment.form.RecipeForm;
+import com.example.reciperestassignment.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +15,29 @@ import java.util.List;
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private final RecipeInstructionService recipeInstructionService;
+    private final IngredientService ingredientService;
+    private final RecipeCategoryService recipeCategoryService;
+    private final RecipeIngredientService recipeIngredientService;
 
     @Autowired
-    public RecipeController(RecipeService recipeService) {
+    public RecipeController(RecipeService recipeService, RecipeInstructionService recipeInstructionService, IngredientService ingredientService, RecipeCategoryService recipeCategoryService, RecipeIngredientService recipeIngredientService) {
         this.recipeService = recipeService;
+        this.recipeInstructionService = recipeInstructionService;
+        this.ingredientService = ingredientService;
+        this.recipeCategoryService = recipeCategoryService;
+        this.recipeIngredientService = recipeIngredientService;
     }
 
     @PostMapping("/api/recipes")
-    public ResponseEntity<RecipeDto> createRecipe(@RequestBody RecipeDto recipeDto){
-        RecipeDto newRecipe = recipeService.create(recipeDto);
+    public ResponseEntity<RecipeDto> createRecipe(@RequestBody RecipeForm recipeForm){
+        RecipeDto newRecipe = recipeService.create(recipeForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(newRecipe);
     }
 
     @PutMapping("/api/recipes/{id}")
-    public ResponseEntity<Void> update(@RequestBody RecipeDto recipeDto, @PathVariable("id") Integer id){
-        recipeService.update(recipeDto,id);
+    public ResponseEntity<Void> update(@RequestBody RecipeForm recipeForm, @PathVariable("id") Integer id){
+        recipeService.update(recipeForm,id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
@@ -40,21 +50,21 @@ public class RecipeController {
 
     @GetMapping("/api/recipes")
     public ResponseEntity<List<RecipeDto>> findAll(){
-        return ResponseEntity.ok(recipeService.findAll());
-
+        List<RecipeDto> allRecipe = recipeService.findAll();
+        return ResponseEntity.ok(allRecipe);
     }
 
     @GetMapping("/api/recipes/category")
     public ResponseEntity<List<RecipeDto>> findAllByCategory(@RequestParam("category") String category){
         return ResponseEntity.ok(recipeService.findAllByCategory(category));
-
     }
 
     @GetMapping("/api/recipes/{id}")
     public ResponseEntity<RecipeDto> findById(@PathVariable("id") Integer id){
         System.out.println("############");
         System.out.println("Id" + id);
-        return ResponseEntity.ok(recipeService.findById(id));
+        RecipeDto recipeById = recipeService.findById(id);
+        return ResponseEntity.ok(recipeById);
     }
 
     @GetMapping("/api/recipes/recipe")
@@ -72,8 +82,14 @@ public class RecipeController {
         return ResponseEntity.ok(recipeService.findByPartOfName(name));
     }
 
+    //-----------------------------------------------------------------
 
 
+    @GetMapping("/api/recipes/category/{categoryId}")
+    public ResponseEntity<RecipeCategoryDto> findCategoryById(@PathVariable("categoryId") Integer recipeCategoryId){
+        RecipeCategoryDto findCategory = recipeCategoryService.findById(recipeCategoryId);
+        return ResponseEntity.ok(findCategory);
+    }
 
 
 
